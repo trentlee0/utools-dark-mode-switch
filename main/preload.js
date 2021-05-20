00270a9c5a17bbd08c3d658784367f554625ef81
TrentLee <trent0@foxmail.com>
2021-05-20 22:29:31 +0800

    Update preload.js

--- main/preload.js
+++ main/preload.js
@@ -6,10 +6,8 @@ window.exports = {
     args: {
       enter: (action) => {
         window.utools.hideMainWindow();
-        exec(`lib/Dark-Light.ps1`, {
-          shell: 'powershell.exe',
-          cwd: __dirname
-        }, (error, stdout, stderr) => {
+        let darkLightPS = '$out = reg.exe query HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize /v SystemUsesLightTheme; $outStr = $out -as [string]; if ($outStr -match \'(SystemUsesLightTheme.+0x)(\\d)\') {$mode = $Matches[2] } else {$mode = 1 }; if ($mode -as [int] -eq 1) {$night = 0 } else {$night = 1 }; reg.exe add HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize /v AppsUseLightTheme /t REG_DWORD /d $night /f; reg.exe add HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize /v SystemUsesLightTheme /t REG_DWORD /d $night /f;';
+        exec(`powershell.exe ${darkLightPS}`, (error, stdout, stderr) => {
           if (error) {
             window.utools.showNotification(`执行的错误: ${error}`);
           }
