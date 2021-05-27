530696f73444fb236e2af12b9859ea81c9f7c840
TrentLee <trent0@foxmail.com>
2021-05-27 23:00:42 +0800

    Update Dark-Light.ps1

--- lib/Dark-Light.ps1
+++ lib/Dark-Light.ps1
@@ -1,17 +1,11 @@
 # 0 为深色模式，1 为浅色模式
-$out = reg.exe query HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme
-$outStr = $out -as [string]
-if ($outStr -match '(SystemUsesLightTheme.+0x)(\d)') {
-    $mode = $Matches[2]
-} else {
-    $mode = 1
-}
-
-if ($mode -as [int] -eq 1) {
-    $night = 0
-} else {
-    $night = 1
-}
-
-reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d $night /f
-reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d $night /f
+$out = reg.exe query HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme;
+[int]$mode = 1;
+if ($out -as [string] -match '(SystemUsesLightTheme.+0x)(\d)')
+{
+    $mode = $Matches[2] -as [int];
+};
+$mode = -not($mode -as [bool]) -as [int];
+$out = reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d $mode /f;
+$out = reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d $mode /f;
+Write-Host '切换完成！'
