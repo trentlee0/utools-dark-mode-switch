5dffb2cfe64bc93d762020305115bdc2b8433e3a
TrentLee <trent0@foxmail.com>
2021-06-01 12:22:06 +0800

    Update Dark-Light.ps1

--- lib/Dark-Light.ps1
+++ lib/Dark-Light.ps1
@@ -6,6 +6,6 @@ if ($out -as [string] -match '(SystemUsesLightTheme.+0x)(\d)')
     $mode = $Matches[2] -as [int];
 };
 $mode = -not($mode -as [bool]) -as [int];
-$out = reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d $mode /f;
-$out = reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d $mode /f;
+reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d $mode /f | Out-Null;
+reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d $mode /f | Out-Null;
 Write-Host '切换完成！'
